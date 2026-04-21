fc4b64979dd07a825d86a36af85e00f34c2d8407
Shirajuana <Sheilamaeveluya@gmail.com>
2026-04-22 07:46:42 +0800

    Pa deploy

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, send_from_directory
-import os
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -12,4 +11,4 @@ def static_files(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
